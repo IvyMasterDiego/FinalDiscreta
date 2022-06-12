6cde78519a96a0f1bceba4355eb9d974dcc54045
IvyMasterDiego <diegolobato82@gmail.com>
2022-06-12 12:13:49 -0400

    Frist Commit

---
+++ GraphGenerator/GraphGenerator/Cvertice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGenerator
+{
+    class Cvertice
+    {
+        public string Valor;
+        public List<CArco> ListaAdyacencia;
+        Dictionary<string, short> _banderas;
+        Dictionary<string, short> _banderasPredert;
+
+
+        public Color Color
+        {
+            get { return color_nodo; }
+            set { color_fuente = value; }
+        }
+
+        public Point Posicion
+        {
+            get { return _posicion; }
+            set { _posicion = value; }
+        }
+
+        public Size Dimensiones
+        {
+            get { return dimensiones; }
+            set
+            {
+                radio = value.Width / 2;
+                dimensiones = value;
+            }
+        }
+
+        static int size = 35;
+        Size dimensiones;
+        Color color_nodo;
+        Color color_fuente;
+        Point _posicion;
+        int radio;
+
+        public Cvertice(string Valor)
+        {
+            this.Valor = Valor;
+            this.ListaAdyacencia = new List<CArco>();
+            this._banderas = new Dictionary<string, short>();
+            this._banderasPredert = new Dictionary<string, short>();
+            this.Color = Color.Green;
+            this.Dimensiones = new Size(size, size);
+            this.FontColor = Color.White;
+        }
+
+    }
+}
