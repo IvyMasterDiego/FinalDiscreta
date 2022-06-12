925d5b5b20e3cbd509665c392607099b3f257823
IvyMasterDiego <diegolobato82@gmail.com>
2022-06-12 17:02:24 -0400

    Agregando distintas clases de los grafos

---
+++ GraphGenerator/GraphGenerator/CArco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphGenerator
+{
+    class CArco
+    {
+        public Cvertice nDestino;
+        public int peso;
+        public float grososr_flecha;
+        public Color color;
+        
+
+        public CArco(Cvertice destino) : this(destino, 1)
+        {
+            this.nDestino = destino;
+        }
+        public CArco(Cvertice destino, int peso)
+        {
+            this.nDestino = destino;
+            this.peso = peso;
+            this.grososr_flecha = 2;
+            this.color = Color.Red;
+        }
+    }
+}
---
+++ GraphGenerator/GraphGenerator/CLista.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGenerator
+{
+    class CLista
+    {
+        private Cvertice aElemento;
+        private CLista aSubLista;
+        private int aPeso;
+
+        public CLista()
+        {
+            aElemento = null;
+            aSubLista = null;
+            aPeso = 0;
+        }
+
+        public CLista(CLista pLista)
+        {
+            if (pLista != null)
+            {
+                aElemento = pLista.aElemento;
+                aSubLista = pLista.aSubLista;
+                aPeso = pLista.aPeso;
+            }
+        }
+
+        public Cvertice Elemento
+        {
+            get
+            {
+                return aElemento;
+            }
+            set
+            {
+                aElemento = value;
+            }
+        }
+        public CLista SubLista
+        {
+            get
+            {
+                return aSubLista;
+            }
+            set { aSubLista = value; }
+        }
+        public int Peso
+        {
+            get
+            {
+                return aPeso;
+            }
+            set
+            {
+                aPeso = value;
+            }
+        }
+
+        public bool EsVacia()
+        {
+            return aElemento == null;
+        }
+
+        public void Agregar(Cvertice pElemento, int pPeso)
+        {
+            if (pElemento != null)
+            {
+                if (aElemento == null)
+                {
+                    aElemento = new Cvertice(pElemento.nombre);
+                    aPeso = pPeso;
+                    aSubLista = new CLista();
+                }
+                else
+                {
+                    if (!ExisteElemento(pElemento))
+                    {
+                        aSubLista.Agregar(pElemento, pPeso);
+                    }
+                }
+            }
+        }
+
+        public void Eliminar(Cvertice pElemento)
+        {
+            if (aElemento != null)
+            {
+                if (aElemento.Equals(pElemento))
+                {
+                    aElemento = aSubLista.aElemento;
+                    aSubLista = aSubLista.SubLista;
+                }
+                else
+                {
+                    aSubLista.Eliminar(pElemento);
+                }
+            }
+        }
+        public int NroElementos()
+        {
+            if (aElemento != null)
+            {
+                return 1 + aSubLista.NroElementos();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object lesimoElemento(int posicion)
+        {
+            if ((posicion > 0) && (posicion <= NroElementos()))
+            {
+                if (posicion == 1)
+                {
+                    return aElemento;
+
+                }
+                else
+                {
+                    return aSubLista.lesimoElemento(posicion - 1);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object lesimoElementoPeso(int posicion)
+        {
+            if ((posicion > 0) && (posicion <= NroElementos()))
+            {
+                if (posicion == 1)
+                {
+                    return aPeso;
+                }
+                else
+                {
+                    return aSubLista.lesimoElementoPeso(posicion - 1);
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool ExisteElemento(Cvertice pElemento)
+        {
+            if ((aElemento != null) && (pElemento != null))
+            {
+                return (aElemento.Equals(pElemento) || (aSubLista.ExisteElemento(pElemento)));
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int PosicionElemento(Cvertice pElemento)
+        {
+            if ((aElemento != null) || (ExisteElemento(pElemento)))
+            {
+                if (aElemento.Equals(pElemento))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 1 + aSubLista.PosicionElemento(pElemento);
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
