7ef7ac65edac92f8cbca43a5deea9dc8fbbd56cb
IvyMasterDiego <diegolobato82@gmail.com>
2022-07-12 08:33:45 -0400

    Cambios de formto y elminando varios archivos

--- RubiksCube/EdgeBlock.cs
+++ RubiksCube/EdgeBlock.cs
@@ -1,34 +1,4 @@
-﻿/////////////////////////////////////////////////////////////////////
-//
-//	Rubik's Cube for beginners.
-//	WPF Application using Visual3D classes and animation.
-//	Source code developed with visual C#.
-//
-//	Granotech Limited
-//	Author: Uzi Granot
-//	Version: 1.0
-//	Date: August 1, 2017
-//	Copyright (C) 2017-2022 Uzi Granot. All Rights Reserved
-//
-//	The Rubik's cube for beginners is a free software.
-//	It is distributed under the Code Project Open License (CPOL 1.02)
-//	agreement. The full text of the CPOL is given in:
-//	https://www.codeproject.com/info/cpol10.aspx
-//	
-//	The main points of CPOL 1.02 subject to the terms of the License are:
-//
-//	Source Code and Executable Files can be used in commercial applications;
-//	Source Code and Executable Files can be redistributed; and
-//	Source Code can be modified to create derivative works.
-//	No claim of suitability, guarantee, or any warranty whatsoever is
-//	provided. The software is provided "as-is".
-//	The Article accompanying the Work may not be distributed or republished
-//	without the Author's consent
-//
-//	For version history please refer to MainWindow.xaml.cs
-/////////////////////////////////////////////////////////////////////
-
-namespace UziRubiksCube
+﻿namespace UziRubiksCube
 	{
 	/// <summary>
 	/// Edge block
